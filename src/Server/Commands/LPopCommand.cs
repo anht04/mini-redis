7c46e8d459c2018099638e2cb38c55126c731789
ANHT <nhatanhtruong20@gmail.com>
2026-07-17 10:09:19 +0700

    feat: multiple elements support functionality for LPOP command

--- src/Server/Commands/LPopCommand.cs
+++ src/Server/Commands/LPopCommand.cs
@@ -13,18 +13,29 @@ namespace MiniRedis.Commands
         public string Execute(List<string> args, Dictionary<RedisEntry, RedisValue> cache)
         {
             var cacheKey = new RedisEntry { Key = args[1] };
+            var numberOfPopItem = 1;
+            if (args.Count > 2)
+            {
+                numberOfPopItem = int.Parse(args[2]);
+            }
             if (cache.TryGetValue(cacheKey, out var value) && !value.IsList)
             {
                 return RESPFormatHelper.FormatErrorString("WRONGTYPE Operation against a key holding the wrong kind of value");
             }
             var parsedValue = value?.AsList();
-            if(parsedValue is null || parsedValue.Count == 0)
+            if (parsedValue is null || parsedValue.Count == 0)
             {
                 return RedisConstants.NullBulkString;
             }
-            var firstValue = parsedValue[0];
-            parsedValue.RemoveAt(0);
-            return RESPFormatHelper.FormatBulkString(firstValue);
+            var result = new List<string>();
+            for (int i = 0; i < numberOfPopItem; i++)
+            {
+                result.Add(parsedValue[i]);
+            }
+            parsedValue.RemoveRange(0, numberOfPopItem);
+            return result.Count > 1
+                ? RESPFormatHelper.FormatArray(result)
+                : RESPFormatHelper.FormatBulkString(result.First());
         }
     }
 }
