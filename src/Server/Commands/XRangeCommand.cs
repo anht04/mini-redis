e6f26e2d42f1153a23a12fcc88695756cb680f1c
ANHT <nhatanhtruong20@gmail.com>
2026-07-21 22:04:42 +0700

    feat: add XRANGE command

---
+++ src/Server/Commands/XRangeCommand.cs
@@ -0,0 +1,18 @@
+﻿using MiniRedis.Data;
+using MiniRedis.Models.GlobalCache;
+using System.Net.Sockets;
+
+namespace MiniRedis.Commands
+{
+    public class XRangeCommand : ICommand
+    {
+        public int Arity => -4;
+
+        public bool IsWriteCommand => false;
+
+        public Task<string> ExecuteAsync(List<string> args, RedisDatabase cache, Socket client)
+        {
+            var cacheKey = new RedisEntry { Key = args[1] };
+        }
+    }
+}
